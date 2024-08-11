aed070a5ff7d917bd88187290770405d3516834c
Saradhi <99667252+Saradhii@users.noreply.github.com>
2024-08-11 20:30:07 +0530

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,6 +10,7 @@ export interface Props {
 
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
   const { title, pubDatetime, modDatetime, description } = frontmatter;
+  const { metaData } = frontmatter;
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
@@ -22,11 +23,11 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
         href={href}
         className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
       >
-        {secHeading ? (
+{/*         {secHeading ? (
           <h2 {...headerProps}>{title}</h2>
         ) : (
           <h3 {...headerProps}>{title}</h3>
-        )}
+        )} */}
       </a>
       <Datetime pubDatetime={pubDatetime} modDatetime={modDatetime} />
       <p>{description}</p>
